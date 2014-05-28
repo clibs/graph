159132ce83d6159906ad52553b5ea8cc2a30c4a5
Jonathan Barronville <jonathan@belairlabs.com>
2014-05-28 17:36:31 -0400

    Blah ...

--- src/graph.c
+++ src/graph.c
@@ -234,7 +234,26 @@ graph_new(
   graph->vertices   = list_new();
   graph->store_type = store_type;
 
-  _graph_item_set_label(graph, label, _GRAPH_ITEM_TYPE_GRAPH);
+  pthread_t set_label_th;
+  struct _graph_item_set_label_th_args * set_label_th_args = _MALLOC(struct _graph_item_set_label_th_args, 1);
+
+  if (! set_label_th_args) { exit(-1); }
+
+  int set_label_th_err = 0;
+
+  set_label_th_args->item  = graph;
+  set_label_th_args->label = label;
+  set_label_th_args->type  = _GRAPH_ITEM_TYPE_GRAPH;
+
+  set_label_th_err = pthread_create(
+    &set_label_th,
+    NULL,
+    _graph_item_set_label_th,
+    set_label_th_args
+  );
+
+  if (set_label_th_err) { exit(-1); }
+
   graph_setup_store(graph);
 
   graph->_auto_inc.edge.available    = 0;
@@ -252,6 +271,12 @@ graph_new(
 
   // graph->edges->match    = graph_edge_match;
   // graph->vertices->match = graph_vertex_match;
+
+  set_label_th_err = pthread_join(set_label_th, NULL);
+
+  if (set_label_th_err) { exit(-1); }
+
+  _FREE(set_label_th_args);
 
   return graph;
 }
